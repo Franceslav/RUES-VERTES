67be95160a4b0f1d77df45d4a13f00f3216f1033
Franceslav <107437303+Franceslav@users.noreply.github.com>
2025-11-18 21:44:53 +0100

    feat: replace favicon with metalogo.svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,7 +108,7 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico", sizes: "any" },
+      { url: "/metalogo.svg", sizes: "any", type: "image/svg+xml" },
       { url: "/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
       { url: "/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
     ],
